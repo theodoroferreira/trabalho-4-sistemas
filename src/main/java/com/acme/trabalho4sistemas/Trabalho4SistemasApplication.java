e196f89dacdd50f76b7bf5b3099e278212ca16b5
Theodoro Ferreira <theogferreira@outlook.com>
2024-11-08 19:09:04 -0300

    commit

---
+++ src/main/java/com/acme/trabalho4sistemas/Trabalho4SistemasApplication.java
@@ -0,0 +1,13 @@
+package com.acme.trabalho4sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho4SistemasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabalho4SistemasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/trabalho4sistemas/Trabalho4SistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.trabalho4sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho4SistemasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
